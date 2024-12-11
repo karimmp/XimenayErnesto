e89bbe35791f2aba26825ac8489a5bc1ad244f09
KarMa <jkarim.mp@gmail.com>
2024-12-11 17:46:04 -0600

    se corrigió imagenes y agregó viajes

--- assets/js/hotels.js
+++ assets/js/hotels.js
@@ -1,59 +1,66 @@
 const HotelsManager = {
     hotels: [
         {
-            name: "La Lupitas",
+            name: "Hotel Boutique Las Lupitas",
             image: "./assets/images/hotels/lupitas.jpg",
-            phone: "",
-            website: "",
-            booking: ""
+            phone: "+52(981)281-9359",
+            website: "https://www.laslupitas.com/",
+            booking: "https://direct-book.com/properties/laslupitashotelboutique"
         },
         {
             name: "Narrativ Loft",
             image: "./assets/images/hotels/lupitas.jpg",
             phone: "",
-            website: "",
+            website: "https://hospitality.narrativ.mx/",
             booking: ""
         },
         {
             name: "Hotel Casa Piedad",
             image: "./assets/images/hotels/lupitas.jpg",
-            phone: "",
-            website: "",
-            booking: ""
+            phone: "+52(981)171-5498",
+            website: "https://hotelcasapiedad.com/",
+            booking: "https://hotels.cloudbeds.com/reservation/Jt9zlk#checkin=2025-03-28&checkout=2025-03-30"
         },
         {
             name: "Hotel El Navegante",
             image: "./assets/images/hotels/lupitas.jpg",
-            phone: "",
-            website: "",
+            phone: "+52(981)144-4112",
+            website: "https://hotelelnavegante.com.mx/",
             booking: ""
         },
         {
             name: "Hacienda Uayamon",
             image: "./assets/images/hotels/lupitas.jpg",
-            phone: "",
-            website: "",
+            phone: "+52(981)8130-530",
+            website: "https://www.ihg.com/spnd/hotels/us/es/uayamon/cpesi/hoteldetail",
             booking: ""
         },
         {
             name: "City Express by Marriot",
             image: "./assets/images/hotels/lupitas.jpg",
-            phone: "",
-            website: "",
-            booking: ""
+            phone: "+52(981)127-3760",
+            website: "https://www.marriott.com/en-us/hotels/cpexp-city-express-campeche/overview/?scid=f2ae0541-1279-4f24-b197-a979c79310b0",
+            booking: "https://www.marriott.com/en-us/hotels/cpexp-city-express-campeche/overview/?scid=f2ae0541-1279-4f24-b197-a979c79310b0"
         },
         {
             name: "Hotel del Paseo",
             image: "./assets/images/hotels/lupitas.jpg",
-            phone: "",
-            website: "",
+            phone: "+52(981)811-0100",
+            website: "https://hoteldelpaseocampeche.com/",
             booking: ""
         },
         {
             name: "Hotel Gamma by Fiesta Inn",
             image: "./assets/images/hotels/lupitas.jpg",
+            phone: "+52(443)310-8137",
+            website: "https://www.fiestamericanatravelty.com/gamma-hoteles/hoteles/gamma-campeche-malecon",
+            booking: ""
+        },
+        {
+            name: "También hay opciones de airbnb",
+            image: "./assets/images/hotels/airbnb.png",
             phone: "",
-            website: "",
+            website: "https://www.airbnb.mx/",
             booking: ""
         }
     ],
